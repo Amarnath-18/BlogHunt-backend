8fd15e4dcc74e826b7dcfb348bcca727a7d72bbd
Amarnath Garai <amarnathgarai2004@gmail.com>
2025-04-23 23:54:55 +0530

    add console

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 
 export const protectRoute = async (req, res, next) => {
   const { token } = req.cookies;
+  console.log(token) ;
   if (!token) {
     return res.status(401).json({ message: "User not authenticated" });
   }
