ca5ad0323dba55f30392cc647dfd72cc76d16153
Amarnath Garai <amarnathgarai2004@gmail.com>
2025-04-24 09:30:40 +0530

    content maxlength increase

--- models/Blog.js
+++ models/Blog.js
@@ -14,7 +14,7 @@ const blogSchema = new mongoose.Schema(
       required: true,
       trim: true,
       minlength: 5,
-      maxlength: 1000,
+      maxlength: 10000,
     },
     image: {
       type: String,
